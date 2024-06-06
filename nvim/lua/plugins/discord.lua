d0c64a7b0a6c87b62093c4ba6d884d74756adfe5
Jonatan Magnusson <jonatan.magnusson@gmail.com>
2024-06-06 10:52:22 +0200

    neovim: Update Trouble to next major, and add Discord presence plugin

---
+++ nvim/lua/plugins/discord.lua
@@ -0,0 +1,18 @@
+return {
+  'andweeb/presence.nvim',
+  config = function()
+    require('presence').setup({
+      enable_line_number = false,
+      log_level = "debug",
+      presence_log_level = "debug",
+
+      editing_text = "Editing file...",
+      file_exporer_text = "Browsing file system...",
+      git_commit_text = "Committing changes",
+      plugin_manager_text = "Managing plugins",
+      reading_text = "Reading file...",
+      workspace_text = "Working in a workspace...",
+      line_number_text = "Line %s out of %s"
+    })
+  end
+}
