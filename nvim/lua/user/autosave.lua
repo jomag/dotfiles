ec528ab36b88dd9ffd15954e3c81062ecbd3439e
Jonatan Magnusson <jonatan.magnusson@gmail.com>
2025-03-06 09:18:16 +0100

    nvim: add autosave on focus lost

---
+++ nvim/lua/user/autosave.lua
@@ -0,0 +1,9 @@
+vim.api.nvim_create_autocmd("FocusLost", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.modified then
+      print("Autosaving modified buffers...")
+      vim.cmd("silent! wa")
+    end
+  end,
+})
