4239158f0df4a2870c3593a401ceec4f0f1382b9
Jonatan Magnusson <jonatan.magnusson@gmail.com>
2025-04-04 21:06:52 +0200

    nvim: darken gutter background in rose-pine theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -37,5 +37,19 @@ return {
     lazy = false,
     priority = 1000,
     name = "rose-pine",
+    config = function()
+      -- Make the gutter stand out a bit.
+      -- Default background of rose-pine-moon is #232136.
+      -- This color is darkened by 10%
+      local gutter_bg = "#1f1e31"
+
+      require("rose-pine").setup({
+        highlight_groups = {
+          LineNr = { bg = gutter_bg },
+          SignColumn = { bg = gutter_bg },
+          CursorLineNr = { bg = gutter_bg }
+        }
+      })
+    end
   }
 }
