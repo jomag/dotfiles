fa42db113d8af4b12de50ccaf917e0c0243c105d
Jonatan Magnusson <jonatan.magnusson@gmail.com>
2025-04-04 21:07:10 +0200

    nvim: disable gitsigns in gutter

--- nvim/lua/user/lazy.lua
+++ nvim/lua/user/lazy.lua
@@ -30,6 +30,7 @@ require('lazy').setup({
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
+    enabled = false,
     opts = {
       -- See `:help gitsigns.txt`
       signs = {
